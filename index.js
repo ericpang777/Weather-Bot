d03db69000b7fe0437c8970613eb82c417058c1a
Eric Pang <eric.pang@live.com>
2018-07-25 10:20:38 -0400

    Add start to package.json

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "Bohbm65lc5"
+  let VERIFY_TOKEN = "verifytoken"
     
   // Parse the query params
   let mode = req.query['hub.mode'];
