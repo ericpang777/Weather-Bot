8bdc3edbcae5d0579bb8892894806ad2d02a84db
Tayef_Shah <tayefshah@gmail.com>
2018-08-18 17:11:53 -0400

    Testing postbacks

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.post('/webhook', function (req, res) {
                 if (messagingEvent.message) {
                     receivedMessage(messagingEvent);
                 } else if (messagingEvent.postback) {
-                    receivedPostback(messagingEvent);
+                    receivedMessage(messagingEvent);
                 } else {
                     console.log("Webhook received unknown messagingEvent: ", messagingEvent);
                 }
