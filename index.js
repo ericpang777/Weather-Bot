c2432606171aa9d7cb0eae129084a426308ab1d9
Eric Pang <eric.pang@live.com>
2018-08-20 14:14:20 -0400

    Test time zone logic

--- index.js
+++ index.js
@@ -171,6 +171,7 @@ function receivedMessage(event) {
 function getForecastArrayIndex(lat, long) {
     var arrayIndex = -1;
     request((TIMEZONE_API_URL+TIMEZONE_API_KEY+"&format=json&by=position&lat="+lat+"&lng="+long), {json: true}, (error, response, data) => {
+        console.log("in request");
         if(error) {
             console.log("Error:", error);
             arrayIndex = -1;
